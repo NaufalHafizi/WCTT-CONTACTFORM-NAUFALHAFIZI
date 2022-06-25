3ae1cc878809d0f51085e74418891484456af22c
NaufalHafizi <naufalhafizi@gmail.com>
2022-06-26 03:32:24 +0800

    created submit form ui

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,65 +1,27 @@
-<script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
-</script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <div class="min-h-screen bg-gray-100 text-gray-800 antialiased px-4 py-6 flex flex-col justify-center sm:py-12">
+    <div class="relative py-3 sm:max-w-xl mx-auto text-center">
+      <span class="text-2xl font-light">Contact Form</span>
+      <div class="relative mt-4 bg-white shadow-md sm:rounded-lg text-left">
+        <div class="h-2 bg-indigo-400 rounded-t-md"></div>
+        <div class="py-6 px-8">
+          
+          <!-- svelte-ignore a11y-label-has-associated-control -->
+          <label class="block font-semibold">Name<label>
+          <input type="text" placeholder="Name" class="border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-md">
+          
+          <label class="block pt-4 font-semibold">Email<label>
+          <input type="text" placeholder="Email" class="border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-md">
+          
+          <label class="block pt-4 font-semibold">Description<label>
+          <textarea placeholder="Description" class="border block w-full px-3 py-1.5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-md"></textarea>
+          
+          <div class="flex justify-center">
+            <button class="mt-4 bg-indigo-500 text-white py-2 px-6 rounded-lg">Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
-</style>
